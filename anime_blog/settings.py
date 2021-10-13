5e0628c6b972e71faca1fe5e27c6fb9bd0cf2e67
Abai <kanybekov.abay.reserve@gmail.com>
2021-10-13 20:04:34 +0600

    static root

--- anime_blog/settings.py
+++ anime_blog/settings.py
@@ -136,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
